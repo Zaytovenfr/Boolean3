967ea8bed12eefba94a22bf071407232a9934ab6
Zavaon Taylor <taylozav000@warren.k12.in.us>
2024-10-22 10:15:24 -0400

    boolean

---
+++ main.py
@@ -0,0 +1,14 @@
+# TODO: Write something in the
+#  input function
+#  instructing the user to enter their age.
+age = int(input("Enter your age: "))
+
+#TODO: Use a relational operator to evaluate is_eligible
+# YOU are are eligible if your age is greater than or equal
+# to 18.
+is_eligible = age >= 18
+
+#TODO: Print the result
+print("Is eligible to vote: ", is_eligible)
+
+
